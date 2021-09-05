7adcda1843f66101d0cb0d563fbc7a071e878c13
mohammedali988 <mohd.os.1998@gmail.com>
2021-09-05 04:12:31 +0300

    ChildToParents

--- picks/src/App.js
+++ picks/src/App.js
@@ -2,9 +2,14 @@ import React from "react"
 import SearchBar from "./component/searchBar";
 
 const App = ()=>{
+
+  const onSearchSubmit = (term) => {
+    console.log(term,"here")
+  }
+
   return (
-    <div> 
-      <SearchBar />
+    <div className="ui container" style={{marginTop:"10px"}}> 
+      <SearchBar onSubmit={onSearchSubmit}/>
     </div>
   )
 }
--- picks/src/component/searchBar.js
+++ picks/src/component/searchBar.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-const SearchBar = ()=>{
+const SearchBar = (props)=>{
+    const[term, setTerm] = useState("")
+
+    const onchange = (e)=>{
+        setTerm(e.target.value)
+    };
+
+    const onsubmit = (e)=>{
+        e.preventDefault();
+        props.onSubmit(term)
+    };
+
     return (
-        <div> Search bar </div>
+        <div className="ui segment"> 
+            <form onSubmit={onsubmit} className="ui form">
+                <div className="field"> 
+                    <label>Search bar</label>
+                    <input 
+                        type="text"
+                        value={term} 
+                        onChange={onchange}
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
 
