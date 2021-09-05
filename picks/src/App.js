676c7075794872bf77383d768fb08bfdb007cb5f
mohammedali988 <mohd.os.1998@gmail.com>
2021-09-05 06:09:58 +0300

    Fetch api

--- picks/src/App.js
+++ picks/src/App.js
@@ -1,15 +1,25 @@
-import React from "react"
+import unsplash from "./api/unsplash";
+import React, { useState } from "react"
 import SearchBar from "./component/searchBar";
 
 const App = ()=>{
 
+  const [image, setImage] = useState([])
+
   const onSearchSubmit = (term) => {
-    console.log(term,"here")
+    unsplash.get(
+      '/photos/',{
+        params:{query:term},
+      }
+    ).then( response => {
+      return setImage(response.data)
+    })
   }
 
   return (
     <div className="ui container" style={{marginTop:"10px"}}> 
       <SearchBar onSubmit={onSearchSubmit}/>
+      We found: {image.length} photos
     </div>
   )
 }
